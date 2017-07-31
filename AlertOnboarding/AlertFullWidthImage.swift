35469e6492c90ead690a2fd687b1882b71d5844b
webdevotion <webdevotion>
2017-07-31 14:47:30 +0200

    helper class to maintain image aspect ratio when taking up the full width

---
+++ AlertOnboarding/AlertFullWidthImage.swift
@@ -0,0 +1,13 @@
+//
+//  AlertFullWidthImage.swift
+//  AlertOnboarding
+//
+//  Created by Bram Plessers on 31/07/2017.
+//  Copyright © 2017 CookMinute. All rights reserved.
+//
+
+import UIKit
+
+class AlertFullWidthImage: UIImage {
+
+}
